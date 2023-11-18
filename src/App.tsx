dfcb82a2e6e880749b76d2aaaa471458c81c0e0a
liquedgit <89082382+liquedgit@users.noreply.github.com>
2023-11-18 12:27:01 +0700

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "primereact/resources/themes/lara-light-indigo/theme.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-red-500">Hello World</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ src/config/firebase.ts
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp} from "firebase/app";
+import {getFirestore} from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAoYvXGc3Oz5bj1lDyTBNDPgq8TZ0UERkE",
+  authDomain: "bcqs-72b21.firebaseapp.com",
+  projectId: "bcqs-72b21",
+  storageBucket: "bcqs-72b21.appspot.com",
+  messagingSenderId: "117322102182",
+  appId: "1:117322102182:web:ea83d1a2a9dd4a1d3b4b67",
+  measurementId: "G-TE94SVK7GH"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
