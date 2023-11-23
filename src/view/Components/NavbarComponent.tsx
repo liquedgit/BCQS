c10e889bd47caaf3d9041b06e6af58d4fd0d712c
lowkeyy420 <minecraftuser6907@gmail.com>
2023-11-24 01:11:05 +0700

    Tweaked navbar

--- src/view/Components/NavbarComponent.tsx
+++ src/view/Components/NavbarComponent.tsx
@@ -2,28 +2,45 @@ import { useNavigate } from "react-router-dom";
 import { auth } from "../../lib/config/firebase";
 import { Link } from "react-router-dom";
 import { useAuth } from "../../hooks/AuthContext";
+import { TENANT_ROLE, USER_ROLE } from "../../lib/config/constant";
 
 export default function Navbar() {
   const nav = useNavigate();
-  const { user } = useAuth();
+  const { user, role } = useAuth();
   return (
     <>
       <div className="navbar bg-base-100 sticky">
         <div className="flex-1">
-          <a className="btn btn-ghost text-xl">Welcome, {user?.email}</a>
+          <Link to={"/"} className="btn btn-ghost text-xl">Home</Link>
+          <p className=" pl-5 text-xl">Welcome, {user?.email}</p>
         </div>
         <div className="flex-none">
           <ul className="menu menu-horizontal px-1">
             <li>
               <details>
-                <summary>Parent</summary>
+                <summary >Menu</summary>
                 <ul className="p-2 bg-base-100">
-                  <li>
-                    <a>Link 1</a>
-                  </li>
-                  <li>
-                    <Link to={"queue"}>Link 2</Link>
-                  </li>
+                  {role === USER_ROLE && (
+                    <>
+                      <li>
+                        <Link className="whitespace-nowrap" to={"/queue"}>My Queue</Link>
+                      </li>
+                    </>
+                  )}
+                  {role === TENANT_ROLE && (
+                    <>
+                      <li>
+                        <Link className="whitespace-nowrap" to={"/queue"}>All Queue</Link>
+                      </li>
+                      <li>
+                        <Link className="whitespace-nowrap" to={"/products"}>My Products</Link>
+                      </li>
+                    </>
+                  )}
+
+
+
+
                 </ul>
               </details>
             </li>
